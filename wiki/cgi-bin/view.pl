9d6ceaf535495c34273090b92eecc245e208c5a4
Eduardo Portugal Portugal <eportugalpor@unsa.edu.pe>
2023-11-25 10:05:56 -0500

    Agregando html y mejorando traductor de Markdown a html

--- wiki/cgi-bin/view.pl
+++ wiki/cgi-bin/view.pl
@@ -15,6 +15,10 @@ $sth->execute($title);
 $dbh->disconnect;
 my $text = $sth->fetchrow_array;
 
+# Dos espacios seguidos y devuelve un salto de línea en el html:
+$text =~ s/( {2,})$/<br>/mg; ## Corregir
+#$text =~ s/  $/<br>/mg;
+
 # Encabezados
 $text =~ s/^(#+)\s+(.*)$/"<h" . length($1) . ">$2<\/h" . length($1) . ">"/meg;
 
@@ -36,6 +40,21 @@ $text =~ s/~~(.*?)~~/<s>$1<\/s>/gs;
 # Código
 #$text =~ s/```(.*?)```/<p><code>$1<\/code><\/p>/gs;
 $text =~ s/```(.*?)```/<code>$1<\/code>/gs;
+
+# Imagenes con URL:
+$text =~ s/!\[(.*?)\]\((https?:\/\/.*?)\)\{width=(\d+) height=(\d+)\}/<img src="$2" alt="$1" width="$3" height="$4">/gs;
+
+# Imagenes locales:
+$text =~ s/!\[(.*?)\]\((.*?)\)/<img src="$2" alt="$1">/gs; ## Corregir
+
+# Citas:
+$text =~ s/^>\s+(.*)$/<blockquote>$1<\/blockquote>/mg;
+
+# Listas desordenadas, reconocen 3 tipos de simbolos, el *, el - y el +:
+$text =~ s/^\s*[-*+]\s+(.*)$/<ul><li>$1<\/li><\/ul>/mg;
+
+# Listas ordenadas:
+$text =~ s/^\s*\d+\.\s+(.*)$/<ol><li>$1<\/li><\/ol>/mg;
 
 # Enlaces
 $text =~ s/\[(.*?)\]\((.*?)\)/<a href="$2">$1<\/a>/gs;
