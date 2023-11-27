427bc5483edef80dd40da4e50257d2d84d86ff39
Eduardo Portugal Portugal <eportugalpor@unsa.edu.pe>
2023-11-27 01:25:54 -0500

    se modifico ligeramente el codigo de list.pl y view.pl

--- wiki/cgi-bin/list.pl
+++ wiki/cgi-bin/list.pl
@@ -17,20 +17,17 @@ print header,
 start_html(-title=>"Lista Paginas Web"),
 h1 ("Nuestras paginas de wiki"),
 ul({-style => "margin-left: -20px;"});
-#ul;
 while (my $title = $sth->fetchrow_array) {
     print li({-style => "margin-bottom: 10px;"},
         a({-href => "view.pl?title=$title", -style => "display: inline-block;"}, "$title"),
         start_form(-action => 'delete.pl', -method => 'get', -style => "display: inline-block;"),
-        hidden(-name => 'title', -value => $title),
-        submit(-type => 'submit', -value => 'X'),
+        input({-type => "hidden", -name => 'title', -value => $title}),
+        input({-type => 'submit', -value => 'X'}),
         end_form,
         start_form(-action => 'edit.pl', -method => 'get', -style => "display: inline-block;"),
-        hidden(-name => 'title', -value => $title),
-        submit(-type => 'submit', -value => 'E'),
-        end_form);
-
-        
+        input({-type => "hidden", -name => 'title', -value => $title}),
+        input({-type => 'submit', -value => 'E'}),
+        end_form); 
 }
 print "</ul>",
 hr,
@@ -38,22 +35,3 @@ a({-href => 'http://localhost/new.html'}, 'Nueva Pagina'),
 br,
 a({-href => 'http://localhost/index.html'}, 'Volver al Inicio'),
 end_html;
-
-    #print li(a({-href => "view.pl?title=$title"}, "$title"));
-
-    #print start_form(-action => 'delete.pl', -method => 'get'),
-    #    hidden(-name => 'title', -value => $title),
-    #    submit(-type => 'submit', -value => 'X'),
-    #    end_form,
-    #    "&nbsp;",  # Espacio entre los botones,
-    #    start_form(-action => 'edit.pl', -method => 'get'),
-    #    hidden(-name => 'title', -value => $title),
-    #    submit(-type => 'submit', -value => 'E'),
-    #    end_form;
-
-        #form({-action => 'delete.pl', -method => 'get'},
-        #    hidden(-name => 'title', -value => $title),
-        #    submit(-type => 'submit', -value => 'X')),
-        #form({-action => 'edit.pl', -method => 'get'},
-        #    hidden(-name => 'title', -value => $title),
-        #    submit(-type => 'submit', -value => 'E')));
--- wiki/cgi-bin/view.pl
+++ wiki/cgi-bin/view.pl
@@ -15,10 +15,6 @@ $sth->execute($title);
 $dbh->disconnect;
 my $text = $sth->fetchrow_array;
 
-# Dos espacios seguidos y devuelve un salto de línea en el html:
-$text =~ s/( {2,})$/<br>/mg; ## Corregir
-#$text =~ s/  $/<br>/mg;
-
 # Encabezados
 $text =~ s/^(#+)\s+(.*)$/"<h" . length($1) . ">$2<\/h" . length($1) . ">"/meg;
 
@@ -38,35 +34,28 @@ $text =~ s/_([^_]+)_/<em>$1<\/em>/gs;
 $text =~ s/~~(.*?)~~/<s>$1<\/s>/gs;
 
 # Código
-#$text =~ s/```(.*?)```/<p><code>$1<\/code><\/p>/gs;
 $text =~ s/```(.*?)```/<code>$1<\/code>/gs;
 
-# Imagenes con URL:
+# Imagenes con URL
 $text =~ s/!\[(.*?)\]\((https?:\/\/.*?)\)\{width=(\d+) height=(\d+)\}/<img src="$2" alt="$1" width="$3" height="$4">/gs;
-
-# Imagenes locales:
-$text =~ s/!\[(.*?)\]\((.*?)\)/<img src="$2" alt="$1">/gs; ## Corregir
 
 # Citas:
 $text =~ s/^>\s+(.*)$/<blockquote>$1<\/blockquote>/mg;
 
-# Listas desordenadas, reconocen 3 tipos de simbolos, el *, el - y el +:
+# Listas desordenadas, reconocen 3 tipos de simbolos ( * , - y + )
 $text =~ s/^\s*[-*+]\s+(.*)$/<ul><li>$1<\/li><\/ul>/mg;
 
-# Listas ordenadas:
+# Listas ordenadas
 $text =~ s/^\s*\d+\.\s+(.*)$/<ol><li>$1<\/li><\/ol>/mg;
 
 # Enlaces
 $text =~ s/\[(.*?)\]\((.*?)\)/<a href="$2">$1<\/a>/gs;
 
 # Párrafos
-#$text =~ s/(.*)/<p>$1<\/p>/gm;
 $text =~ s/(^\s*.+\n)/<p>$1<\/p>/mg;
 
 print header,
 start_html(-title=>$title),
 a({-href => 'list.pl'}, 'Retroceder'),
 $text,
-br,
-textarea(-id => 'cont', -name => 'text', -rows => '10', -cols => '50', -value => $text),
 end_html;
